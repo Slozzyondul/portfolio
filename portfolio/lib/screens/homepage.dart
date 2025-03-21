cf1a184b367be60804f835096a066dc70d79424f
Solomon <solomonondula@gmail.com>
2025-03-21 14:46:31 +0300

    fnal adjustment of the appbar

--- portfolio/lib/screens/homepage.dart
+++ portfolio/lib/screens/homepage.dart
@@ -11,7 +11,37 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        shape: Border.all(width: 2),
+        backgroundColor: Colors.grey,
+        elevation: 10,
+        title: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                alignment: Alignment.center,
+                'assets/images/logo.JPG',
+                height: MediaQuery.of(context).size.height * 0.05,
+                width: MediaQuery.of(context).size.width * 0.15,
+              ),
+            ),
+            SizedBox(width: MediaQuery.of(context).size.width * 0.15),
+            Flexible(
+              child: Text(
+                'Solomon Ondula Omusinde',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                  overflow: TextOverflow.ellipsis,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
